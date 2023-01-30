102d3eeac3f076887ca93292edfce36c258257d1
EduardoGF312 <traxer3122@gmail.com>
2023-01-30 13:55:37 -0600

    vapi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components';
-import {GoogleApiWrapper} from 'google-maps-react';
+import MapContainer from './google.jsx';
 
 const Button = styled.button`
   /* Adapt the colors based on primary prop */ 
@@ -12,15 +12,17 @@ function App() {
     <div className="App">
       <h1>Botones</h1>
       <h2>Eduardo Garcia Flores</h2>
-      <a href='https://utd.edu.mx/' target="_blank">
+      <a href='https://utd.edu.mx/' target={ "_blank" }>
         <Button className='utd'><span>UTD</span></Button>
       </a>
-      <a href='https://github.com/EduardoGF312/Practica_componentes.git' target="_blank">
+      <a href='https://github.com/EduardoGF312/Practica_componentes.git' target={ "_blank" }>
         <Button className='git'><span>GitHub</span></Button>
       </a>
-      <a href='https://instagram.com/eduardo_g03?igshid=ZDdkNTZiNTM=' target="_blank">
+      <a href='https://instagram.com/eduardo_g03?igshid=ZDdkNTZiNTM=' target={ "_blank" }>
         <Button className='insta'><span>Instagram</span></Button>
       </a>
+      
+      <MapContainer/>
       
     </div>
   );
